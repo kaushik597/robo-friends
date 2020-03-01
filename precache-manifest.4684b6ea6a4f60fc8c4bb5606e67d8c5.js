87c289656a0630edcc46c0a73d163b821a3fd53c
kaushik <kaushikyadavjallam005@gmail.com>
2020-03-01 17:48:16 +0530

    Updates

---
+++ precache-manifest.4684b6ea6a4f60fc8c4bb5606e67d8c5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fd85360c025826475ad157be3c0b9ffe",
+    "url": "/robo-friends/index.html"
+  },
+  {
+    "revision": "cf9af96a5217b5e79e22",
+    "url": "/robo-friends/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "aa86a9f550c36249eb73",
+    "url": "/robo-friends/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "cf9af96a5217b5e79e22",
+    "url": "/robo-friends/static/js/2.e558f83d.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/robo-friends/static/js/2.e558f83d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aa86a9f550c36249eb73",
+    "url": "/robo-friends/static/js/main.8a78f35e.chunk.js"
+  },
+  {
+    "revision": "0ea51cd6cf6bc27b8f40",
+    "url": "/robo-friends/static/js/runtime-main.ae471ddb.js"
+  }
+]);
